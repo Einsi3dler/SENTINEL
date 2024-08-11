cdd9d421925eda3d3bf1208c65e0019dd4f57bbc
Sylvester Divine <sylvesterdivine@outlook.com>
2024-08-11 15:15:13 +0100

    changes to fix

--- models/classes.py
+++ models/classes.py
@@ -1,13 +1,11 @@
 """This Contains the models"""
 from models.transaction import Transaction
 from models.user import User
-from models.leaderboard import Leaderboard
 from models.messages import Message
 
 class db_models:
     __classes = {"Transaction": Transaction, 
                  "User": User,
-                 "Leaderboard": Leaderboard,
                  "Message": Message
                  }
 
